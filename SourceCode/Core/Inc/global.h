5f5b7bbcf2c37b1b4770bb92bccc96f38e7fc027
nabicold03 <binh.vunabi2509@hcmut.edu.vn>
2023-10-30 14:29:33 +0700

    no message

--- SourceCode/Core/Inc/global.h
+++ SourceCode/Core/Inc/global.h
@@ -13,9 +13,10 @@
 #include"main.h"
 
 extern int status;
-#define INIT		1
-#define AUTO_RED	2
-#define AUTO_YELLOW	3
-#define AUTO_GREEN	4
+#define INIT	1
+#define Gr1Re2	2
+#define Ye1Re2	3
+#define Re1Gr2	4
+#define Re1Ye2	5
 
 #endif /* INC_GLOBAL_H_ */
--- SourceCode/Core/Src/fsm.c
+++ SourceCode/Core/Src/fsm.c
@@ -10,17 +10,49 @@ void fsm_run(){
 	switch(status){
 		case INIT:
 			//TODO
-			status=AUTO_RED;
-			setTimer1(500);
-			break;
-		case AUTO_RED:
 
+			//setup environment
+			status=Gr1Re2;
+			setTimer1(3000);	//set timer 1 cho auto_red chay trong 5s
+			setTimer2(1000);	//set timer 2 de display len led
 			break;
-		case AUTO_YELLOW:
+		case Gr1_Re2:
+			//TODO
 
+			//setup environment
+			if(timer1_flag==1){
+				status=Ye1_Re2;
+				setTimer1(2000);
+				setTimer2(1000);
+			}
 			break;
-		case AUTO_GREEN:
+		case Ye1_Re2:
+			//TODO
 
+			//setup environment
+			if(timer1_flag==1){
+				status=Re1_Gr2;
+				setTimer1(3000);
+				setTimer2(1000);
+			}
+			break;
+		case Re1_Gr2:
+			//TODO
+
+			//setup environment
+			if(timer1_flag==1){
+				status=Re1_Ye2;
+				setTimer1(2000);
+				setTimer2(1000);
+			}
+			break;
+		case Re1_Ye2:
+			//TODO
+
+			//setup environment
+			if(timer1_flag==1){
+				status=INIT;
+			}
 			break;
 		default:
 			break;
